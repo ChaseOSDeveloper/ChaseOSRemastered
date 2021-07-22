598dee57fe402e03a6611f9e232fa68a80812dad
somedeveloper123 <87677459+somedeveloper123@users.noreply.github.com>
2021-07-22 19:10:52 -0400

    added some stuffs

--- ChaseOS/src/impl/kernel/kernel.c
+++ ChaseOS/src/impl/kernel/kernel.c
@@ -1,3 +1,7 @@
+#include "print.h"
+
 void oscall() {
-    printf("hi there");
+    print_clear();
+    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+    print_str("Welcome to Chase OS");
 }
